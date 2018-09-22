1306c10e6a74bb9900c86239a8d2fbece8591462
Hannibal-ad-Portas <jconroy12@gmail.com>
2018-09-22 02:30:09 -0400

    Added files for each question
    The files are header files so that it can be included.
    The files are included in stdafx.h
    The main file group3.cc includes stdafx.h
    
    Also added a make file to compile on linux operating sytems

---
+++ DollarChange.h
@@ -0,0 +1,16 @@
+/*	=============================================================================
+ *	This program asks the user for a number of coins, and calculates wheather those
+ *	coins equal exactly one US Dollar. If the coins do add up to a dollar, the 
+ *	program congradulates the user, otherwise the program tells the user if the 
+ *	entered coins add up to more or less than a dollar, and by how much.
+ *
+ *	The program only asks for the amount of pennies, nickels, dimes, quarters,
+ *	and half dollars.
+ *
+ *	===========================================================================*/
+
+int DollarChange () {
+	//TODO
+	
+	return 0;
+}
---
+++ MagicDates.h
@@ -0,0 +1,19 @@
+/*	=============================================================================
+ *	This function asks the user to enter a date.
+ *	The function then determines whether the month times the year is equal to the
+ *	year. If it is then the date is magic, and the program tells the user. If the
+ *	date is not magic the program tells the user that the date is not magic.
+ *
+ *	The date nneds to be entered in neumerica form, and the year should be two-digits
+ *
+ *	Example
+ *	May 14, 1970 is magic. This is because 5 * 14 = 70
+ *
+ *	===========================================================================*/
+
+
+int MagicDate () {
+	//TODO
+	
+	return 0;
+}
