be80cfd3da7ba005367d1318834c5c88e1b12d79
Kennyjp2016 <43482403+Kennyjp2016@users.noreply.github.com>
2018-09-26 12:06:48 -0400

    Used the NSSeating.cpp header

--- Nationals_Income_Calculator.cpp
+++ Nationals_Income_Calculator.cpp
@@ -1,6 +1,17 @@
-﻿/*	This program calculates the income for the National's team baseball stadium
-*	It only allows positive numbers as inputs and block letters, words and numbers greater than the number of seats.
-*/
+/*	=============================================================================
+ *	This function calculates the income from ticket sales for a special game.
+ *	The function gets ticket amounts from the user, and displays the total.
+ *
+ *	There are three different classes of seats, costing different amounts:
+ *		Class A: $15
+ *		Class B: $12
+ *		Class C: $9
+ *
+ *	The Total is displayed in fixed point notation, with two decimal points of
+ *	precision. The decimal point is always displayed.
+ *
+ *	All values are in American Dollars
+ *	===========================================================================*/
 
 #include "pch.h"
 #include <iostream>
