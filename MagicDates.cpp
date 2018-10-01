73f4808bfb952c07b00d978af99da9a0d1202a3c
Hannibal-ad-Portas <jconroy12@gmail.com>
2018-10-01 02:07:18 -0400

    Magic dates now works
    Like the other functions it asks the user for input and performs
    calcumation on that input.

--- MagicDates.cpp
+++ MagicDates.cpp
@@ -4,15 +4,50 @@
  *	year. If it is then the date is magic, and the program tells the user. If the
  *	date is not magic the program tells the user that the date is not magic.
  *
- *	The date nneds to be entered in neumerica form, and the year should be two-digits
+ *	The date needs to be entered in numeric form, and the year should be two-digits
  *
  *	Example
  *	May 14, 1970 is magic. This is because 5 * 14 = 70
  *
  *	===========================================================================*/
 #include <iostream>
+#include "util.h"
+using namespace std;
+
+bool isMagic (int month, int day, int year) {
+	int product = month * day;
+	if (product == year) {
+		return true;
+	}
+	return false;
+}
 
 void MagicDate () {
-	//TODO
-	std::cout << "Magic Dates \n";
+	bool exit = false;
+	int month, day, year = 0;
+	while (exit == false) {
+		cls();
+		cout << "\t\tMAGIC DATES\n" << "This function determines if a date is magic.\n";
+		cout << "Please enter all dates in numeric form,";
+		cout << " and only the last two digits of the year.\n";
+		cout << "Please enter your month: ";
+		month = goodInt(1, 12);
+		cout << "Please enter the day: ";
+		day = goodInt(1, 31);
+		cout << "Please enter the year: ";
+		year = goodInt(0, 99);
+
+		if (isMagic(month, day, year) == true) {
+			cout << "\nThat date is magic\n\n";
+		} else {
+			cout << "\nThat date is not magic\n\n";
+		}
+		cout << "Enter 0 if you want to stay.\nEnter any other number to return to the menu:";
+		int x;
+		cin >> x;
+		if (x != 0) {
+			exit = true;
+		}
+	}
+	return;
 }
