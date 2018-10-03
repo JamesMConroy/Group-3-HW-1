2764d4445fc8b083dc364427c1de84d2bb4f147f
James Conroy <jconroy12@gmail.com>
2018-10-03 01:15:35 -0400

    Add files via upload
    Sections.h contains prototypes for the parts of the program

---
+++ Sections.h
@@ -0,0 +1,7 @@
+#pragma once
+//Be sure to update the prototypes below if you change the definitions
+
+void AvgRainFall();
+void DollarChange();
+void MagicDate();
+void Nationals_Income_Calculator();
